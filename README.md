0b59e64ee73b69368e70ba613d0eb57d25869672
xyxy8080 <386025225@qq.com>
2024-06-03 14:57:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# MetaSD: A Unified Framework for Scalable Downscaling of Meteorological Variables in Diverse Situations
+## Accepted by 
+**The official repository with Pytorch**
+
+Our paper can be downloaded from [paper](https://arxiv.org/abs/2404.17611).
+
+## Introduction
+
+MetaSD is a novel approach proposed in this paper that addresses the challenges of downscaling meteorological variables by leveraging meta-learning. Unlike existing downscaling methods that focus on individual variables in isolation, MetaSD aims to provide a unified framework capable of downscaling diverse meteorological variables derived from different numerical models and spatiotemporal scales. By training on variables such as temperature, wind, surface pressure, and total precipitation from ERA5 and GFS, MetaSD demonstrates its ability to capture the interconnectedness among various variables. This generalized downscaling model outperforms existing top downscaling methods, as evidenced by both quantitative and qualitative assessments.
+<img src='./figure/MetaSD.jpg'/>
+
+
+## Installation
+**Clone this repo:**
+
+```bash
+git clone https://github.com/neuralchen/MetaSD.git
+cd MetaSD
+```
+**Dependencies:**
+
+- python3.6+
+- pytorch
+- scikit-image, scipy, tqdm, imageio, numpy, opencv-python
+
+## Training
+
+To train the model use the following command:
+
+```
+python train.py
+```
+
+
+## Datasets
+
+Our data can be downloaded from [data](https://drive.google.com/drive/folders/1Mf2QBt-hAFUzdzkpEFVcEHX74Qv89t5v).
+
+
+## Inference with a pretrained MetaSD model
+### Pretrained Models
+- Baidu Netdisk (百度网盘)：https://pan.baidu.com/s/1ui-GSbAQLuTyOmxBlAQZVg 
+- Extraction Code (提取码)：lspg
+
+## Ackownledgements
+This code is built based on [MAML](https://github.com/sanghyun-son/EDSR-PyTorch) and [EDSR](https://github.com/sanghyun-son/EDSR-PyTorch). We thank the authors for sharing the codes.
+
+## Citation
+If you use this paper work in your research or work, please cite our paper:
+```
+@ARTICLE{2024arXiv240417611H,
+       author = {{Hu}, Jing and {Zhang}, Honghu and {Zheng}, Peng and {Mu}, Jialin and {Huang}, Xiaomeng and {Wu}, Xi},
+        title = "{MetaSD: A Unified Framework for Scalable Downscaling of Meteorological Variables in Diverse Situations}",
+         year = 2024,
+          eid = {arXiv:2404.17611},
+       eprint = {2404.17611},
+}
+```
---
+++ train.py
@@ -0,0 +1,72 @@
+import argparse
+import os
+import time
+
+import  torch
+from myData import Mydata,Get_data
+from    meta import Meta
+from torch.utils.data import DataLoader
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+
+torch.manual_seed(111)
+torch.cuda.manual_seed_all(111)
+np.random.seed(111)
+
+
+def main(args):
+
+    device = torch.device(args.cuda)
+    maml = Meta(args).to(device)
+
+    tmp = filter(lambda x: x.requires_grad, maml.parameters())
+    num = sum(map(lambda x: np.prod(x.shape), tmp))
+    print('Total trainable tensors:', num)
+
+    data = Mydata()
+    get_data = Get_data()
+    dataloader = DataLoader(data, batch_size=args.meta_batch, shuffle=True)
+
+    glob_step = 0
+    writer = SummaryWriter(log_dir='logs')
+    val_i=0
+    for epoch in range(args.epoches):
+        s=time.time()
+        l=[]
+        for idx, x in enumerate(dataloader):
+            T=get_data.in_data(tasks=x,spt=args.spt,qry=args.qry)
+            tt0 = maml(T,epoch,idx)
+            glob_step = glob_step + 1
+            l.append(tt0)
+        loss=np.mean(np.array(l),axis=0)
+        # 记录训练损失
+        writer.add_scalar('Train/Loss', loss[-1], epoch)
+        print('epoch:', epoch, ' \ttime:',time.time()-s,'\ttraining loss:', loss)
+
+        if  epoch%2000==0:
+            T=get_data.in_fine()
+            val_loss=maml.fine(T,epoch)
+            # 记录验证损失
+            writer.add_scalar('Validation/Loss', val_loss,val_i)
+            val_i+=1
+    # 关闭SummaryWriter对象
+    writer.close()
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--epoches', type=int, help='epoch number', default=2000000000)
+    argparser.add_argument('--cuda', type=str, help='device', default='cuda:0')
+
+    argparser.add_argument('--meta_batch', type=int, help='Task num for a single update,  max=len(Tasks)', default=1)
+    argparser.add_argument('--update_step', type=int, help='step of updates per task in the inner loop', default=1)
+
+    argparser.add_argument('--spt', type=int, help='support set', default=10)
+    argparser.add_argument('--qry', type=int, help='query set', default=10)
+
+    argparser.add_argument('--meta_lr', type=float, help='meta-level outer learning rate', default=1e-4)
+    argparser.add_argument('--task_lr', type=float, help='task-level inner update learning rate', default=1e-2)
+
+    args = argparser.parse_args()
+    main(args)
